f7446aa9c9e97852bd992f3abb4a71058b19747f
Ignacio Baixas <iobaixas@gmail.com>
2015-06-08 12:24:01 -0300

    feat(cli): improves command output on wrong input

--- bin/crabtrap.bin.js
+++ bin/crabtrap.bin.js
@@ -7,82 +7,102 @@ var localhost = require('../lib/domains/localhost'),
 
 // Globals
 
-var SOURCE = null,
-	PORT = 4000,
-	MEMENTO = new Memento(),
-	VIRTUAL = null;
+var DEFAULT_PORT = 4000;
+
+function renderHelp() {
+	var pjson = require('../package.json');
+
+	console.log('Crabtrap Proxy ' + pjson.version);
+	console.log('####################\n');
+
+	console.log('USAGE');
+	console.log('\tcrabtrap [mode] [memento_path] [options]');
+
+	console.log('MODES');
+	console.log('\tcapture - proxy will capture requests into [memento_path]');
+	console.log('\treplay  - proxy will replay requests from [memento_path]');
+	console.log('\tpass    - pass through mode, no capture or replay is performed.');
+
+	console.log('COMMANDS');
+	console.log('\t--port    - Proxy port, defaults to ' + DEFAULT_PORT);
+	console.log('\t--log     - Log level, valid levels are debug, info, warn and error, defaults to warn.');
+	console.log('\t--virtual - Virtual domain asset path, if set, folder contents are made available at www.crabtrap.io.');
+
+	console.log('\n');
+}
 
 (function() {
-	if(process.argv.length < 2) throw 'Must provide a proxy mode';
-	MODE = process.argv[2];
-	var i = 3;
 
-	if(MODE != 'pass') {
-		if(process.argv.length < 3) throw 'Must provide a bucket path';
-		SOURCE = process.argv[3];
+	if(process.argv.length < 2) return renderHelp();
+
+	var proxyMode = process.argv[2],
+		proxyPort = DEFAULT_PORT,
+		memento = new Memento(),
+		mementoPath = null,
+		virtualPath = null,
+		i = 3;
+
+	if(proxyMode != 'pass') {
+		if(process.argv.length < 3) return renderHelp();
+		mementoPath = process.argv[3];
 		i = 4;
 	}
 
 	for(; i < process.argv.length; i++) {
 		var parts = process.argv[i].split('=');
 		switch(parts[0]) {
-			case '--port': PORT = parseInt(parts[1], 10); break;
-			case '--quiet': PORT = parseInt(parts[1], 10); break;
-			case '--virtual': VIRTUAL = parts[1]; break;
+			case '--port': proxyPort = parseInt(parts[1], 10); break;
+			case '--log': log.setLevel(parts[1]); break;
+			case '--virtual': virtualPath = parts[1]; break;
 			default: throw 'Invalid option ' + parts[0];
 		}
 	}
-})();
 
-function buildProxy() {
-	var proxy = new Proxy(PORT);
+	var proxy = new Proxy(proxyPort);
 	proxy.setHandler('localhost', localhost.buildHandler());
 
-	switch(MODE) {
+	switch(proxyMode) {
 		case 'pass':
 			proxy.setHandler('proxy', require('../lib/modes/pass').buildHandler());
 			break;
 		case 'capture':
-			proxy.setHandler('proxy', require('../lib/modes/capture').buildHandler(MEMENTO));
+			proxy.setHandler('proxy', require('../lib/modes/capture').buildHandler(memento));
 			break;
 		case 'replay':
-			proxy.setHandler('proxy', require('../lib/modes/replay').buildHandler(MEMENTO));
+			proxy.setHandler('proxy', require('../lib/modes/replay').buildHandler(memento));
 			break;
-		default: throw 'Invalid proxy mode';
+		default: return renderHelp();
 	}
 
-	if(VIRTUAL) {
-		var scraper = require('../lib/virtual/plugin');
-		scraper.setupProxy(proxy, VIRTUAL);
+	if(virtualPath) {
+		var virtualServer = require('../lib/virtual/plugin');
+		virtualServer.setupProxy(proxy, virtualPath);
 	}
 
-	return proxy;
-}
+	console.log("Starting crabtrap! mode: " + proxyMode);
 
-var PROXY = buildProxy();
+	if(proxyMode == 'replay') {
+		memento.loadFrom(mementoPath, proxy.listen.bind(proxy));
+	} else {
+		proxy.listen();
+	}
 
-console.log("Starting crabtrap! mode: " + MODE);
+	var exiting = false;
+	function finishGracefully() {
+		if(exiting) return;
+		exiting = true;
 
-if(MODE == 'replay') {
-	MEMENTO.loadFrom(SOURCE, PROXY.listen.bind(PROXY));
-} else {
-	PROXY.listen();
-}
+		console.log("Shutting down crabtrap!");
+		proxy.close(function() {
+			if(proxyMode == 'capture') {
+				memento.saveTo(mementoPath, process.exit.bind(process));
+			} else {
+				process.exit();
+			}
+		});
+	}
 
-var EXITING = false;
-function finishGracefully() {
-	if(EXITING) return;
-	EXITING = true;
+	process.on('SIGTERM', finishGracefully);
+	process.on('SIGINT', finishGracefully);
 
-	console.log("Shutting down crabtrap!");
-	PROXY.close(function() {
-		if(MODE == 'capture') {
-			MEMENTO.saveTo(SOURCE, process.exit.bind(process));
-		} else {
-			process.exit();
-		}
-	});
-}
-
-process.on('SIGTERM', finishGracefully);
-process.on('SIGINT', finishGracefully);
+})();
